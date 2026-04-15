8d6681eea13f188e9e61017cc958806784a788f8
lindatomaskova <linda.tomaskova@email.cz>
2026-04-15 20:58:05 +0200

    Create hra.cpp

---
+++ hra.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int hrac_zivoty;
+    int hrac_maximalni_pocet_zivotu;
+    int cena_za_leceni_ve_vesnici1;
+    int cena_za_leceni_ve_vesnici2;
+    int cena_za_dovednosti_ve_vesnici1;
+    int cena_za_dovednosti_ve_vesnici2;
+    int cena_za_vylepseni_utoku_ve_vesnici1;
+    int cena_za_vylepseni_utoku_ve_vesnici2;
+    int cena_za_vylepseni_energie_ve_vesnici1;
+    int cena_za_vylepseni_energie_ve_vesnici2;
+    int pocet_zlata;
+    int odmena_zlato;
+    int monstrum_zivoty;
+    int mini_boss_zivoty;
+    int boss_zivoty;
+    int pocet_nepratel_v_suboji;
+
+    cout << "VITEJ VE HRE!";
+
+
+
+}
