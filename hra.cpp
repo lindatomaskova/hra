291072e0399c9d3c0a98e0d14727008f36ee36c2
lindatomaskova <linda.tomaskova@email.cz>
2026-04-19 22:05:21 +0200

    Update hra.cpp

--- hra.cpp
+++ hra.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main() {
+    srand(time(0));
     int hrac_zivoty;
     int hrac_maximalni_pocet_zivotu;
     int cena_za_leceni_ve_vesnici1;
@@ -18,9 +20,90 @@ int main() {
     int mini_boss_zivoty;
     int boss_zivoty;
     int pocet_nepratel_v_suboji;
+    int vyber_classy;
+    int zacatek_1;
+    char dalsi;
 
-    cout << "VITEJ VE HRE!";
+    cout << "===VITEJ VE HRE!===" << endl;
+    cout << "Tvym ukolem je prezit proti bossum a monstrum." << endl;
+    cout << "\nCo vsechno ve hre muzes najit: " << endl;
+    cout << "1) Monstra - budes proti nim muset bojovat, aby jsi dostal zkusenosti." << endl;
+    cout << "2) Vesnice - zde si budes moc kupovat spoustu potrebnych veci a taky to slouzi jako takove tvoje 'bezpecne' misto." << endl;
+    cout << "\nCo muzes delat ve vesnici: " << endl;
+    cout << "1) doplnit si zivoty." << endl;
+    cout << "2) zvysit svuj maximalni pocet zivotu." << endl;
+    cout << "3) vylepsit si svoje schopnosti nebo utok." << endl;
+    cout << "\nJak funguji souboje: " << endl;
+    cout << "1) pokud budes bojovat proti monstrum, jsi na tahu prvni." << endl;
+    cout << "2) pokud budes bojovat proti mini-bossum nebo proti hlavnimu bossu, je na tahu prvni vzdycky on." << endl;
+    cout << "Takze si vzdycky dobre rozmysli, co chces udelat, protoze jakmile ztratis vsechny zivoty HRA KONCI. " << endl;
+    cout << "\nTvuj hlavni cil je: PORAZIT HLAVNIHO BOSSE." << endl;
 
+    do{
+    cout << "Zmacni 1 pro pokracovani: ";
+    cin >> zacatek_1;
 
+    if (zacatek_1 != 1)
+        cout << "Spatna volba, zkus to znova." << endl;
+    }while (zacatek_1 != 1);
 
+    cout << "\nNa zacatku hry si vyber clasu: " << endl;
+    cout << "1) Paladin:" << endl;
+    cout << "   - max. pocet zivotu: " << endl;
+    cout << "   - aktualni zivoty: " << endl;
+    cout << "   - max. energie: " << endl;
+    cout << "   - aktualni energie: " << endl;
+    cout << "   - pocet zlatych minci: " << endl;
+    cout << "   - level: " << endl;
+    cout << "   - zkusenosti: " << endl;
+    cout << "   - utok: " << endl;
+    cout << "   - schopnosti: " << endl;
+    cout << "Pro dalsi classu zmacni 'd' pro dalsi classu: ";
+    cin >> dalsi;
+
+    if (dalsi == 'd'){
+    cout << "2) Lovec:" << endl;
+    cout << "   - max. pocet zivotu: " << endl;
+    cout << "   - aktualni zivoty: " << endl;
+    cout << "   - max. energie: " << endl;
+    cout << "   - aktualni energie: " << endl;
+    cout << "   - pocet zlatych minci: " << endl;
+    cout << "   - level: " << endl;
+    cout << "   - zkusenosti: " << endl;
+    cout << "   - utok: " << endl;
+    cout << "   - schopnosti: " << endl;
+    cout << "Pro dalsi classu zmacni 'd' pro dalsi classu: ";
+    cin >> dalsi;
+    }
+    
+    if (dalsi == 'd'){
+    cout << "3) Mag:" << endl;
+    cout << "   - max. pocet zivotu: " << endl;
+    cout << "   - aktualni zivoty: " << endl;
+    cout << "   - max. energie: " << endl;
+    cout << "   - aktualni energie: " << endl;
+    cout << "   - pocet zlatych minci: " << endl;
+    cout << "   - level: " << endl;
+    cout << "   - zkusenosti: " << endl;
+    cout << "   - utok: " << endl;
+    cout << "   - schopnosti: " << endl;
+    cout << "Pro dalsi classu zmacni 'd' pro dalsi classu: ";
+    cin >> dalsi;
+    }
+
+    if (dalsi == 'd'){
+    cout << "4) Warlock:" << endl;
+    cout << "   - max. pocet zivotu: " << endl;
+    cout << "   - aktualni zivoty: " << endl;
+    cout << "   - max. energie: " << endl;
+    cout << "   - aktualni energie: " << endl;
+    cout << "   - pocet zlatych minci: " << endl;
+    cout << "   - level: " << endl;
+    cout << "   - zkusenosti: " << endl;
+    cout << "   - utok: " << endl;
+    cout << "   - schopnosti: " << endl;
+
+    cout << "Napis svuj vyber: ";
+    cin >> vyber_classy;
+    }
 }
